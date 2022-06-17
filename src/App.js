c34b85b749e03edcd2b509955aebda6061046053
Jeremy Vollant <j.vollant@gmail.com>
2022-06-17 15:32:50 +0200

    new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
 import './App.css';
 
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItems from './components/ExpenseItems';
 
 function App() {
   const expenses = [
@@ -26,38 +26,8 @@ function App() {
     },
   ];
   return (
-    // <div className="App">
     <div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header> */}
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}></ExpenseItem>
+      <ExpenseItems expenses={expenses} />
     </div>
   );
 }
