aa5a802ea670cf4b40d168e16bf44c8b24b65966
Jeremy Vollant <j.vollant@gmail.com>
2022-06-20 01:36:51 +0200

    data binding

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense addExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,9 +2,18 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const onSaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.addExpense(expenseData);
+    console.log(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
     </div>
   );
 };
